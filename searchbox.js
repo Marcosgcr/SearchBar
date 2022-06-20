5ca502328b3a3c8c7292138bd77ec8376c0c0d54
Marcos Guimarães <41563209+Marcosgcr@users.noreply.github.com>
2022-06-20 20:22:45 +0100

    Create searchbox.js

---
+++ searchbox.js
@@ -0,0 +1,11 @@
+
+var a = document.getElementById('Enter')
+var texto = document.getElementById("Procure")
+a.addEventListener('click',Procura)
+  
+function Procura(){
+  if (texto.innerHTML.lenght == 0){
+  window.alert("Não foi possível buscar, você não inseriu nenhuma palavra na caixa de busca")
+  }else{
+  window.alert("Inserir aqui a procura")
+  }
