30c5d8f72c9ef9d99e1c78bc011b098de3456d9f
Marcos Guimarães <41563209+Marcosgcr@users.noreply.github.com>
2022-06-21 09:32:58 +0100

    Update searchbox.js

--- searchbox.js
+++ searchbox.js
@@ -1,11 +1,8 @@
-
-var a = document.getElementById('Enter')
-var texto = document.getElementById("Procure")
-a.addEventListener('click',Procura)
-  
-function Procura(){
-  if (texto.value.length == 0){
+function funProcura(){
+  if (texto.value.length == 0){  
     window.alert("Não foi possível buscar, você não inseriu nenhuma palavra na caixa de busca")
   }else{
     window.alert("Inserir aqui a procura")
   }
+}
+//As variaveis estão no html e o listener também
