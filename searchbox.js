ef08ba0598a9ca8c7deb7f067281a77ff802ed63
Marcos Guimarães <41563209+Marcosgcr@users.noreply.github.com>
2022-06-21 09:10:57 +0100

    Update searchbox.js

--- searchbox.js
+++ searchbox.js
@@ -4,7 +4,7 @@ var texto = document.getElementById("Procure")
 a.addEventListener('click',Procura)
   
 function Procura(){
-  if (texto.innerHTML.lenght == 0){
+  if (texto.value.length == 0){
     window.alert("Não foi possível buscar, você não inseriu nenhuma palavra na caixa de busca")
   }else{
     window.alert("Inserir aqui a procura")
